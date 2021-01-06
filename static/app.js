38a06f185c6e1e918f9daa52004f9ac4b4932f55
Maria Quinton <mariaquinton@Marias-MBP.lan1>
2021-01-05 18:15:14 -0600

    Working through issues

--- static/app.js
+++ static/app.js
@@ -1,28 +1,52 @@
-// Create function to plot data for our charts
-function plot(id) {
+function getPlot(id) {
+ 
+    // Using the D3 library to load our samples.json file
+    d3.json('data/samples.json').then((bellyButtonData) => {
+        console.log(bellyButtonData)
 
-// Using the D3 library to load our samples.json file
-    d3.json('data/samples.json').then((bellyButtonData)=> {
-        var data = bellyButtonData;
-        console.log(data)
-
-        var washingFreq = data.metadata.map(wash => wash.washingFreq)
+        var washingFreq = bellyButtonData.metadata.map(wash => wash.washingFreq)
         console.log(`The washing frequency is: ${washingFreq}`)
 
-        var sampleValues = data.samples.filter(sample => sample.id.toString() === id)[0];
+        var sampleValues = bellyButtonData.sampleValues.filter(sample => sample.id.toString() === id)[0];
         console.log(sampleValues);
 
         // refine by top 10
         var topTen = sampleValues.sample_values.slice(0, 10).reverse();
 
-        var topTenOtu = (sampleValues.otu_ids.slice(0, 10)).reverse();
+        var OTU_topTen = (sampleValues.otu_ids.slice(0, 10)).reverse();
 
-        var IdOtu = topTenOtu.map(data => 'OTU' + data)
-        console.log(IdOtu)
+        var OTU_id = OTU_topTen.map(data => 'OTU' + data)
+        console.log(OTU_topTen)
 
         // grab the labels for top 10 for the plots
         var labels = sampleValues.otu_labels.slice(0, 10);
         console.log(sampleValues)
 
-    }
-};
+        // create the traces for my plots
+        var traceBar = {
+            x: topTen,
+            y: IdOtu,
+            text: labels,
+            type: 'bar',
+            orientation: 'h',
+        };
+
+        //data
+        var data = [traceBar];
+
+        //layout
+        var layout = {
+            title: 'Top Ten OTU Values',
+            margin: {
+                l: 100,
+                r: 100,
+                t: 100,
+                b: 100,
+            }
+        };
+
+        Plotly.newPlot('bar', data, layout);
+
+    });
+
+}  
