f3d4e5249aae3c88413469bf46a6018e1148ccb6
Maria Quinton <mariaquinton@Marias-MacBook-Pro.local>
2021-01-06 07:39:44 -0600

    Guage Chart

--- static/app.js
+++ static/app.js
@@ -58,6 +58,53 @@ function getPlot(id) {
             text: sampleValues.otu_labels
         };
 
+        var layoutBubble = {
+           xaxis:{title: 'OTU ID'},
+           height: 600,
+           width: 1000 
+        };
+
+        var dataBubble = [traceBubble];
+
+        Plotly.newPlot('bubble', dataBubble, layoutBubble);
+
+        var dataGuage = [
+            {
+                domain: { x: [0, 1], y: [0, 1] },
+                value: washingFreq,
+                title: {text: 'Washing Frequency'},
+                type: 'indicator',
+                mode: 'guage+number',
+                guage: {
+                    axis: { range: [null, 14] },
+                    steps: [
+                    { range: [0, 2], color: 'pink' },
+                    { range: [2, 4], color: 'navy' },
+                    { range: [4, 6], color: 'yellow' },
+                    { range: [6, 8], color: 'royal' },
+                    { range: [8, 9], color: 'green' },
+                    { range: [9, 10], color: 'red' },
+                    { range: [10, 11], color: 'cyan' },
+                    { range: [11, 12], color: 'purple' },
+                    { range: [12, 13], color: 'orange' },
+                    { range: [13, 14], color: 'lime' }
+                    ],
+                    threshold: {
+                        line: {color: 'black', width: 4 },
+                        thickness: 0.75,
+                        value: 13
+                    }
+                }
+            }
+        ];
+
+        var layoutGuage = { 
+            width: 600, 
+            height: 450, 
+            margin: { t: 0, b: 0 }
+        };
+        Plotly.newPlot('guage', dataGuage, layoutGuage);
+
     });
 
 }  
